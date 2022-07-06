9a0f17bc5926ab2cada3ee1958b6ab0f7b639df0
williamjbf <williamjbf@gmail.com>
2022-07-05 22:38:15 -0300

    improving error responses for bean validator

--- src/main/java/com/github/williamjbf/moneyapi/exceptionhandler/MoneyResponseEntityExceptionHandler.java
+++ src/main/java/com/github/williamjbf/moneyapi/exceptionhandler/MoneyResponseEntityExceptionHandler.java
@@ -7,9 +7,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice
 public class MoneyResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -25,7 +32,29 @@ public class MoneyResponseEntityExceptionHandler extends ResponseEntityException
             WebRequest request) {
         String userMessage = messageSource.getMessage("invalid.message", null, LocaleContextHolder.getLocale());
         String devMessage = ex.getCause().toString();
-        return handleExceptionInternal(ex,new Erro(userMessage,devMessage),headers, HttpStatus.BAD_REQUEST, request);
+
+        List<Erro> erros = Arrays.asList(new Erro(userMessage,devMessage));
+        return handleExceptionInternal(ex,erros,headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<Erro> erros = createErrorList(ex.getBindingResult());
+        return handleExceptionInternal(ex, erros,headers, status, request);
+    }
+
+    private List<Erro> createErrorList(BindingResult bindingResult){
+        List<Erro> erroList = new ArrayList<>();
+
+        for (FieldError fieldError: bindingResult.getFieldErrors()){
+            String userMessage = messageSource.getMessage(fieldError,LocaleContextHolder.getLocale());
+            String devMessage = fieldError.toString();
+
+            erroList.add(new Erro(userMessage,devMessage));
+        }
+
+        return erroList;
     }
 
     public static class Erro{

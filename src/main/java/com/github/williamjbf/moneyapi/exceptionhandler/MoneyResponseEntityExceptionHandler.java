560edf26f19686bcdee38af2c90f94b4d0f91fe7
williamjbf <williamjbf@gmail.com>
2022-07-07 10:04:12 -0300

    improving error responses for Empty Result Data Access Exception

--- src/main/java/com/github/williamjbf/moneyapi/exceptionhandler/MoneyResponseEntityExceptionHandler.java
+++ src/main/java/com/github/williamjbf/moneyapi/exceptionhandler/MoneyResponseEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package com.github.williamjbf.moneyapi.exceptionhandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -30,18 +33,41 @@ public class MoneyResponseEntityExceptionHandler extends ResponseEntityException
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
-        String userMessage = messageSource.getMessage("invalid.message", null, LocaleContextHolder.getLocale());
+        String userMessage = messageSource.getMessage(
+                "invalid.message",
+                null,
+                LocaleContextHolder.getLocale());
         String devMessage = ex.getCause().toString();
 
-        List<Erro> erros = Arrays.asList(new Erro(userMessage,devMessage));
+        List<Erro> erros = List.of(new Erro(userMessage, devMessage));
         return handleExceptionInternal(ex,erros,headers, HttpStatus.BAD_REQUEST, request);
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
 
         List<Erro> erros = createErrorList(ex.getBindingResult());
         return handleExceptionInternal(ex, erros,headers, status, request);
+    }
+
+    @ExceptionHandler({EmptyResultDataAccessException.class})
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
+
+        String userMessage = messageSource.getMessage(
+                "resource.not-found",
+                null,
+                LocaleContextHolder.getLocale());
+
+        String devMessage = ex.toString();
+
+        List<Erro> erros = List.of(new Erro(userMessage, devMessage));
+
+        return handleExceptionInternal(ex,erros,new HttpHeaders(),HttpStatus.NOT_FOUND, request);
+
     }
 
     private List<Erro> createErrorList(BindingResult bindingResult){

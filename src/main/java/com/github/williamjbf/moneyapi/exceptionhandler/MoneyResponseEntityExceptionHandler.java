3de494bcbd14b99392cfe97a7dd31feda95e497f
williamjbf <williamjbf@gmail.com>
2022-07-08 20:04:42 -0300

    resolved DataIntegrityViolationException when sending non-existent fkey to posting model category or person

--- src/main/java/com/github/williamjbf/moneyapi/exceptionhandler/MoneyResponseEntityExceptionHandler.java
+++ src/main/java/com/github/williamjbf/moneyapi/exceptionhandler/MoneyResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.github.williamjbf.moneyapi.exceptionhandler;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,12 +15,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @ControllerAdvice
@@ -67,6 +67,22 @@ public class MoneyResponseEntityExceptionHandler extends ResponseEntityException
         List<Erro> erros = List.of(new Erro(userMessage, devMessage));
 
         return handleExceptionInternal(ex,erros,new HttpHeaders(),HttpStatus.NOT_FOUND, request);
+
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+
+        String userMessage = messageSource.getMessage(
+                "resource.operation-not-allowed",
+                null,
+                LocaleContextHolder.getLocale());
+
+        String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+
+        List<Erro> erros = List.of(new Erro(userMessage, devMessage));
+
+        return handleExceptionInternal(ex,erros,new HttpHeaders(),HttpStatus.BAD_REQUEST, request);
 
     }
 

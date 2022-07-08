5cedaf745fc8266edadb087b374dd30006742b06
williamjbf <williamjbf@gmail.com>
2022-07-07 22:12:53 -0300

    added GET method to find posting by id in PostingResource

--- src/main/java/com/github/williamjbf/moneyapi/resource/PostingResource.java
+++ src/main/java/com/github/williamjbf/moneyapi/resource/PostingResource.java
@@ -2,11 +2,14 @@ package com.github.williamjbf.moneyapi.resource;
 
 import com.github.williamjbf.moneyapi.model.Posting;
 import com.github.williamjbf.moneyapi.repository.PostingRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/postings")
@@ -21,5 +24,11 @@ public class PostingResource {
     @GetMapping
     public List<Posting> list(){
         return postingRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Posting> findById(@PathVariable Long id){
+        Optional<Posting> posting = postingRepository.findById(id);
+        return posting.isPresent() ? ResponseEntity.ok(posting.get()) : ResponseEntity.notFound().build();
     }
 }
